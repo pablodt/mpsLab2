2730b091ab580723fa90ab4218b56257945051dc
pablodt <pablo.duran.t@uma.es>
2022-03-22 23:14:19 +0100

    Operaciones complejas implementadas

--- src/main/java/org/Lab2/DoubleLinkedListQueue.java
+++ src/main/java/org/Lab2/DoubleLinkedListQueue.java
@@ -1,5 +1,8 @@
 package org.Lab2;
 
+import java.util.Comparator;
+import java.util.Deque;
+
 public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
 
     private DequeNode<T> first, last;
@@ -82,6 +85,122 @@ public class DoubleLinkedListQueue<T> implements DoubleEndedQueue<T> {
     @Override
     public int size() {
         return doSize(first);
+    }
+
+    @Override
+    public DequeNode<T> getAt(int position) {
+        if (position > 0) {
+            if (position+1 > this.size()) {
+                return null;
+            } else {
+                int i = 1;
+                DequeNode<T> result = first.getNext();
+                while (i < position) {
+                    result = result.getNext();
+                    i++;
+                }
+
+                return result;
+            }
+        } else {
+            return first;
+        }
+    }
+
+    @Override
+    public DequeNode<T> find(T item) {
+        DequeNode<T> result = first;
+        int i = 0;
+        boolean ok = false;
+        while (i < this.size() && !ok) {
+            if (result.getItem().equals(item)) {
+                ok = true;
+            }
+            result = result.getNext();
+            i++;
+        }
+
+        if (!ok) {
+            result = null;
+        }
+
+        return result;
+    }
+
+    @Override
+    public void delete(DequeNode<T> node) {
+        DequeNode<T> n = first;
+        while (!n.equals(last)) {
+            if (n.equals(node)) {
+                if (n.isFirstNode()) {
+                    first = n.getNext();
+                    n.setNext(null);
+                    first.setPrevious(null);
+                } else if (n.isLastNode()) {
+                    last = n.getPrevious();
+                    last.setNext(null);
+                    n.setPrevious(null);
+                } else {
+                    n.getPrevious().setNext(n.getNext());
+                    n.getNext().setPrevious(n.getPrevious());
+                    n.setPrevious(null);
+                    n.setNext(null);
+                }
+            }
+            n = n.getNext();
+        }
+    }
+
+    @Override
+    public void sort(Comparator<T> comparator) {
+        mergeSort(this, comparator);
+    }
+
+    private void mergeSort(DoubleLinkedListQueue<T> doubleLinkedListQueue, Comparator<T> comparator) {
+        if (doubleLinkedListQueue == null) {
+            return;
+        }
+
+        if (doubleLinkedListQueue.size() > 1) {
+            int mid = doubleLinkedListQueue.size()/2;
+
+            DoubleLinkedListQueue<T> left = new DoubleLinkedListQueue<>();
+            for (int i = 0; i < mid; i++) {
+                left.append(doubleLinkedListQueue.getAt(i));
+            }
+
+            DoubleLinkedListQueue<T> right = new DoubleLinkedListQueue<>();
+            for (int i = mid; i < doubleLinkedListQueue.size(); i++) {
+                right.append(doubleLinkedListQueue.getAt(i));
+            }
+
+            mergeSort(left, comparator);
+            mergeSort(right, comparator);
+
+            int i = 0;
+            int j = 0;
+            DoubleLinkedListQueue<T> list = new DoubleLinkedListQueue<>();
+
+            while (i < left.size() && j < right.size()) {
+                if (comparator.compare(left.getAt(i).getItem(), right.getAt(j).getItem()) < 0) {
+                    list.append(left.getAt(i));
+                    i++;
+                } else {
+                    list.append(right.getAt(i));
+                    j++;
+                }
+            }
+
+            while (i < left.size()) {
+                list.append(left.getAt(i));
+                i++;
+            }
+
+            while (j < right.size()) {
+                list.append(right.getAt(i));
+                j++;
+            }
+        }
     }
 
     private int doSize(DequeNode<T> node) {
