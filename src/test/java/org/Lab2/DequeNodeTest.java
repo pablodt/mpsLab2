f7a7fe5ba834f65c3d40243aa77a59a8946d4a6f
pablodt <pablo.duran.t@uma.es>
2022-03-22 11:21:02 +0100

    All tests

--- src/test/java/org/Lab2/DequeNodeTest.java
+++ src/test/java/org/Lab2/DequeNodeTest.java
@@ -15,11 +15,33 @@ class DequeNodeTest<T> {
     }
 
     @Test
+    public void shouldGetItem() {
+        Integer expectedValue = 10;
+        DequeNode<Integer> node = new DequeNode<>(10, null, null);
+        Integer obtainedValue = node.getItem();
+        assertEquals(expectedValue, obtainedValue);
+    }
+    @Test
+    public void shouldGetNextItem() {
+        Integer expectedValue = 15;
+        DequeNode<Integer> node = new DequeNode<>(10, new DequeNode<>(15, null, null), null);
+        Integer obtainedValue = node.getNext().getItem();
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldGetPreviousItem() {
+        Integer expectedValue = 2;
+        DequeNode<Integer> node = new DequeNode<>(10, null, new DequeNode<>(2, null, null));
+        Integer obtainedValue = node.getPrevious().getItem();
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
     public void shouldSetItemChangeItemOfNode() {
         Integer expectedValue = 10;
         dequeNode.setItem(10);
         Integer obtainedValue = dequeNode.getItem();
-
         assertEquals(expectedValue, obtainedValue);
     }
 
@@ -29,8 +51,37 @@ class DequeNodeTest<T> {
         DequeNode<Integer> expectedValue = node;
         dequeNode.setNext(node);
         DequeNode<Integer> obtainedValue = dequeNode.getNext();
+        assertEquals(expectedValue, obtainedValue);
+    }
+
+    @Test
+    public void shouldSetNextChangePreviousOfNode() {
+        DequeNode<Integer> node = new DequeNode<>(10, null, null);
+        DequeNode<Integer> expectedValue = node;
+        dequeNode.setPrevious(node);
+        DequeNode<Integer> obtainedValue = dequeNode.getPrevious();
 
         assertEquals(expectedValue, obtainedValue);
     }
 
+    @Test
+    public void shouldbeFirstNodePreviousisNull() {
+        DequeNode<Integer> node = new DequeNode<>(10, null, null);
+        DequeNode<Integer> expectedValue = node;
+        assertTrue(expectedValue.isFirstNode());
+    }
+
+    @Test
+    public void shouldbeLastNodeNextisNull() {
+        DequeNode<Integer> node = new DequeNode<>(10, null, null);
+        DequeNode<Integer> expectedValue = node;
+        assertTrue(expectedValue.isLastNode());
+    }
+
+    @Test
+    public void shouldnotBeTerminalNodeNextAndPreviousNotNull() {
+        DequeNode<Integer> node = new DequeNode<>(10, new DequeNode<>(15, null, null), new DequeNode<>(6, null, null) );
+        DequeNode<Integer> expectedValue = node;
+        assertTrue(expectedValue.isNotATerminalNode());
+    }
 }
